a08c8403634c8aae8cdc60d3c651761d3d97b8cc
Ahdyns <161012528+Ahdyns@users.noreply.github.com>
2024-05-02 10:37:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-476bffdb-c7d0-40a7-9cae-61b51b87944f/
 
 
-echo "$curl_output" >> Update.tsx
 
 
-echo "Check Update.tsx." 
+
